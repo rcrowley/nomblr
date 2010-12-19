a125af629a86eeff2f5adf538f9bf8a9904c2a29
Richard Crowley <r@rcrowley.org>
2010-12-19 00:49:25 +0000

    A note about database indexes.

--- recipes/models.py
+++ recipes/models.py
@@ -3,7 +3,13 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Recipe(models.Model):
-    # TODO Create an index on owner and slug.
+    """
+    A single recipe, known on the site as a "nom."
+
+    This needs some help from the database:
+
+        CREATE UNIQUE INDEX uri ON recipes_recipe (owner_id, slug);
+    """
     owner = models.ForeignKey(User, related_name='recipes')
     slug = models.SlugField(max_length=255, db_index=False)
     name = models.CharField(max_length=255)
