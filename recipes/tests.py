379cc6ee8e0d499ab6061ac414e34f9f37ed09f8
Richard Crowley <r@rcrowley.org>
2010-12-20 18:34:00 +0000

    Mostly complete recipes testing.

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from django.db import connection
+from django.db import IntegrityError
 from django.test import Client
 
 import forms
@@ -6,12 +9,89 @@ import search_indexes
 import templatetags.mdown
 import views
 
-# TODO Test forms.
+def setup():
+    connection.cursor().execute(
+        'CREATE UNIQUE INDEX uri ON recipes_recipe (owner_id, slug);')
 
-# TODO Test models.
+def test_RecipeForm_valid():
+    user = User.objects.get(username='tester')
+    form = forms.RecipeForm(user, {'name': 'Test recipe',
+                                   'text': 'Test ingredients and directions.'})
+    assert form.is_valid()
+
+def test_RecipeForm_missing_name():
+    user = User.objects.get(username='tester')
+    form = forms.RecipeForm(user, {'text': 'Test ingredients and directions.'})
+    assert not form.is_valid()
+    assert 'name' in form.errors
+
+def test_RecipeForm_missing_text():
+    user = User.objects.get(username='tester')
+    form = forms.RecipeForm(user, {'name': 'Test recipe'})
+    assert not form.is_valid()
+    assert 'text' in form.errors
+
+def test_Recipe_create():
+    user = User.objects.get(username='tester')
+    recipe = models.Recipe.objects.create(
+        owner=user,
+        name='Test recipe',
+        text='Test ingredients and directions.')
+    assert recipe.id is not None
+    assert 'test-recipe' == recipe.slug
+    try:
+        models.Recipe.objects.create(
+            owner=user,
+            name='Test recipe',
+            text='Test ingredients and directions.')
+        assert False
+    except IntegrityError:
+        assert True
+
+# TODO Test models further.
 
 # TODO Test search_indexes.
 
-# TODO Test templatetags.mdown.
+def test_mdown():
+    assert '<ul>\n<li>foo</li>\n</ul>\n<ol>\n<li>bar</li>\n</ol>' \
+        == templatetags.mdown.mdown('* foo\n\n1. bar')
 
-# TODO Test views.
+def test_GET_recipes():
+    c = Client()
+    c.login(username='tester', password='password')
+    response = c.get('/tester/')
+    assert 200 == response.status_code
+
+def test_GET_missing_recipes():
+    c = Client()
+    c.login(username='tester', password='password')
+    response = c.get('/missing/')
+    assert 404 == response.status_code
+
+def test_GET_recipe():
+    c = Client()
+    c.login(username='tester', password='password')
+    response = c.get('/tester/foo-bar/')
+    assert 200 == response.status_code
+
+def test_GET_missing_recipe():
+    c = Client()
+    c.login(username='tester', password='password')
+    response = c.get('/tester/missing/')
+    assert 404 == response.status_code
+    response = c.get('/missing/missing/')
+    assert 404 == response.status_code
+
+def test_POST_recipe():
+    c = Client()
+    c.login(username='tester', password='password')
+    response = c.post('/tester/foo-bar/',
+                      {'name': 'Foo bar',
+                       'text': '* foo\n* bar\n\n1. baz\n1. bang'})
+    assert 200 == response.status_code
+
+def test_POST_invalid_recipe():
+    c = Client()
+    c.login(username='tester', password='password')
+    response = c.post('/tester/foo-bar/', {'name': 'Foo bar'})
+    assert 200 == response.status_code
