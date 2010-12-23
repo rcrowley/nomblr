66c4c30d74808966e73bd70b2783f4e6f01fa476
Richard Crowley <r@rcrowley.org>
2010-12-23 02:04:05 +0000

    Use EmailForm and UsernameForm in SignupForm.
    Bless the Django that this works as expected.

--- forms.py
+++ forms.py
@@ -1,15 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 
-class SignupForm(forms.ModelForm):
+import account.forms
+
+class SignupForm(account.forms.EmailForm, account.forms.UsernameForm):
     """
     Signup, taken mostly from django.contrib.auth.forms.UserCreationForm.
     """
-    email = forms.EmailField(label='Email')
-    username = forms.RegexField(label='Username',
-                                max_length=30,
-                                regex=r'^[\w.-]{2,30}$',
-                                error_messages={'invalid': 'Required.  2 to 30 letters, numbers, periods, and dashes.'})
     password1 = forms.CharField(label='Password',
                                 widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirm password',
@@ -18,22 +15,6 @@ class SignupForm(forms.ModelForm):
     class Meta(object):
         model = User
         fields = ('email', 'username', 'password1', 'password2')
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        try:
-            User.objects.get(email=email)
-        except User.DoesNotExist:
-            return email
-        raise forms.ValidationError('{0} already exists.'.format(email))
-
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return username
-        raise forms.ValidationError('{0} already exists.'.format(username))
 
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1', '')
