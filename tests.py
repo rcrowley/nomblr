05b293e9d808bf502803dea1fce9e73a651aef55
Richard Crowley <r@rcrowley.org>
2010-12-29 01:07:18 +0000

    Tests for invite codes.

--- tests.py
+++ tests.py
@@ -7,13 +7,15 @@ def test_SignupForm_valid():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'username': 'test',
                              'password1': 'password',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert form.is_valid()
 
 def test_SignupForm_missing_email():
     form = forms.SignupForm({'username': 'test',
                              'password1': 'password',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'email' in form.errors
 
@@ -21,14 +23,16 @@ def test_SignupForm_invalid_email():
     form = forms.SignupForm({'email': 'invalid email',
                              'username': 'test',
                              'password1': 'password',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'email' in form.errors
 
 def test_SignupForm_missing_username():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'password1': 'password',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'username' in form.errors
 
@@ -36,7 +40,8 @@ def test_SignupForm_invalid_username():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'username': 'invalid username',
                              'password1': 'password',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'username' in form.errors
 
@@ -44,7 +49,8 @@ def test_SignupForm_long_username():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'username': 'test_test_test_test_test_test_test',
                              'password1': 'password',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'username' in form.errors
 
@@ -52,21 +58,24 @@ def test_SignupForm_short_username():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'username': 't',
                              'password1': 'password',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'username' in form.errors
 
 def test_SignupForm_missing_password1():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'username': 'test',
-                             'password2': 'password'})
+                             'password2': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'password1' in form.errors
 
 def test_SignupForm_missing_password2():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'username': 'test',
-                             'password1': 'password'})
+                             'password1': 'password',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'password2' in form.errors
 
@@ -74,9 +83,27 @@ def test_SignupForm_mismatch_password():
     form = forms.SignupForm({'email': 'test@nomblr.com',
                              'username': 'test',
                              'password1': 'password1',
-                             'password2': 'password2'})
+                             'password2': 'password2',
+                             'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert not form.is_valid()
     assert 'password2' in form.errors
+
+def test_SignupForm_missing_invite_code():
+    form = forms.SignupForm({'email': 'test@nomblr.com',
+                             'username': 'test',
+                             'password1': 'password',
+                             'password2': 'password'})
+    assert not form.is_valid()
+    assert 'invite_code' in form.errors
+
+def test_SignupForm_invalid_invite_code():
+    form = forms.SignupForm({'email': 'test@nomblr.com',
+                             'username': 'test',
+                             'password1': 'password',
+                             'password2': 'password',
+                             'invite_code': 'invalid invite code'})
+    assert not form.is_valid()
+    assert 'invite_code' in form.errors
 
 def test_GET_index():
     c = Client()
@@ -126,7 +153,8 @@ def test_POST_signup():
     response = c.post('/signup/', {'email': 'test@nomblr.com',
                             'username': 'test',
                             'password1': 'password',
-                            'password2': 'password'})
+                            'password2': 'password',
+                            'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert 302 == response.status_code
     assert 'http://testserver/' == response['Location']
 
@@ -135,7 +163,8 @@ def test_POST_invalid_signup():
     response = c.post('/signup/', {'email': 'test@nomblr.com',
                             'username': 'test',
                             'password1': 'password1',
-                            'password2': 'password2'})
+                            'password2': 'password2',
+                            'invite_code': 'aa073e50954855d0533476ad002cce721d1b830d'})
     assert 200 == response.status_code
 
 def test_GET_logout():
